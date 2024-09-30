1e455cde5437c741305996d61e73693229b7424d
steven <43816687+medranosteven@users.noreply.github.com>
2024-09-29 21:11:48 -0600

    delete-linea

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MinervaController;
-use App\Http\Controllers\MinervaLaController;
 use App\Http\Controllers\MinervaOverlayController;
 
 Route::get('/', [MinervaController::class, 'index'])->name('minerva.home');
