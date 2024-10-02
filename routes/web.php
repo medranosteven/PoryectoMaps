132537994e549b4ba4b59238652efa1c3796b9c4
steven <43816687+medranosteven@users.noreply.github.com>
2024-10-01 23:16:27 -0600

    web

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MinervaController;
+use App\Http\Controllers\MinervaLaController;
 use App\Http\Controllers\MinervaOverlayController;
 
+// Ruta principal
 Route::get('/', [MinervaController::class, 'index'])->name('minerva.home');
 
-
+// Ruta para la página principal
 Route::get('/minerva', [MinervaController::class, 'index'])->name('minerva');
-Route::get('/minerva-la', [MinervaLaController::class, 'index'])->name('minerva-la');
 
+// Ruta para mostrar aulas
+Route::get('/minerva-la/aula/{id}', [MinervaLaController::class, 'showAula'])->name('minerva-la.aula');
+
+// Ruta para mostrar referencias
+Route::get('/minerva-la/referencia/{id}', [MinervaLaController::class, 'showReferencia'])->name('minerva-la.referencia');
+
+// Ruta para otro overlay (por si tienes alguna funcionalidad adicional)
 Route::get('/minerva-overley', [MinervaOverlayController::class, 'index'])->name('minerva-overley');
-
-// Ruta para Zonas
-Route::get('/zonas', [MinervaController::class, 'index'])->name('zonas.index');
-
-// Ruta para Aulas
-Route::get('/aulas', [MinervaController::class, 'getAulas'])->name('aulas.index');
